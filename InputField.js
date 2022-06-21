28583029172a2e26700b05c69d778fc085e18632
Zetsu-24 <96690273+Zetsu-24@users.noreply.github.com>
2022-06-21 19:11:26 +0530

    Add files via upload

---
+++ InputField.js
@@ -0,0 +1,37 @@
+
+/*
+function Fibonacci(n) {
+
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2) {
+        output = [0, 1];
+    }
+    else {
+
+        for (var i = 2; i <= n; i++) {
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+
+    return output;
+}
+*/
+
+let i = 5;
+
+while (i >= 0) {
+    console.log(i);
+    i--;
+}
+
+
+
+
+
+
+
+
+
